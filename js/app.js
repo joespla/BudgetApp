b4b076fe5e10979fa2503256aed8d9ca7e38136b
Jorge Espinosa <joespla2000@gmail.com>
2018-05-20 13:58:02 -0500

    Percentages UI created

--- js/app.js
+++ js/app.js
@@ -138,7 +138,8 @@ var UIController = (function() {
     incomeLabel : '.budget__income--value',
     expensesLabel : '.budget__expenses--value',
     percentageLabel : '.budget__expenses--percentage',
-    container: '.container'
+    container: '.container',
+    expensesPercentageLabel : '.item__percentage'
   };
 
   return {
@@ -193,6 +194,21 @@ var UIController = (function() {
         document.querySelector(DOMStrings.percentageLabel).textContent = '---';
       }
     },
+    displayPercentages : function(percentages) {
+      var fields = document.querySelectorAll(DOMStrings.expensesPercentageLabel);
+      var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+      nodeListForEach(fields, function(current, index) {
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + '%';
+        } else {
+          current.textContent = '---';
+        }
+      });
+    },
     getDOMstrings : function() {
       return DOMStrings;
     },
@@ -233,7 +249,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     // Read percentage function from the budget Controller
     var percentages = budgetCtrl.getPercentages();
     // update the UI with the new percentages
-    console.log(percentages);
+    UICtrl.displayPercentages(percentages);
   };
 
   var ctrlAddItem = function() {
