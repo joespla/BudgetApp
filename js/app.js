42a3cf85b9086cccb46737405c5cc860a2ceab91
Jorge Espinosa <joespla2000@gmail.com>
2018-05-21 01:14:40 -0500

    GUI displaying month and year

--- js/app.js
+++ js/app.js
@@ -139,7 +139,8 @@ var UIController = (function() {
     expensesLabel : '.budget__expenses--value',
     percentageLabel : '.budget__expenses--percentage',
     container: '.container',
-    expensesPercentageLabel : '.item__percentage'
+    expensesPercentageLabel : '.item__percentage',
+    dateLabel : '.budget__title--month',
   };
 
   var formatNumber = function(num, type) {
@@ -229,6 +230,14 @@ var UIController = (function() {
           current.textContent = '---';
         }
       });
+    },
+    displayMonth : function() {
+      var now, year, month, months;
+      now = new Date();
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = now.getMonth();
+      year = now.getFullYear();
+      document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
     },
     getDOMstrings : function() {
       return DOMStrings;
@@ -322,6 +331,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init : function() {
       console.log('Application has started');
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget : 0,
         totalInc : 0,
